a87409672e5cbe6416cd19ce4eef91b9248a9952
Kyle <agronick@gmail.com>
2023-09-11 15:10:37 -0400

    Fix icon not displaying when label is set

--- app/src/main/java/com/aatorque/stats/TorqueGauge.kt
+++ app/src/main/java/com/aatorque/stats/TorqueGauge.kt
@@ -157,7 +157,6 @@ class TorqueGauge : Fragment(){
 
         typedArray2.recycle()
         // get min/max values and unit from torque
-        val iconName = data.getDrawableName()
 
         if (data.pid == null) {
             setupClock(
@@ -175,7 +174,7 @@ class TorqueGauge : Fragment(){
         } else {
             setupClock(
                 mIcon,
-                if (!data.display.showLabel && iconName != null) iconName else "ic_none",
+                data.getDrawableName() ?: "ic_none",
                 if (data.display.showLabel) data.display.label else "",
                 mClock,
                 false,
