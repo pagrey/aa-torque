415250eed73a75c48eb65cd63b983ab74cff08c4
Kyle <agronick@gmail.com>
2023-10-07 12:40:21 -0400

    fix: fix crash on log export

--- app/src/main/java/com/aatorque/stats/CacheLogTree.kt
+++ app/src/main/java/com/aatorque/stats/CacheLogTree.kt
@@ -23,42 +23,36 @@ class CacheLogTree: Timber.DebugTree() {
     )
 
     val logCache = ArrayDeque<LogDesc>(KEEP_LOGS)
-    val logLock = Mutex()
 
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        GlobalScope.launch {
-            if (BuildConfig.DEBUG) {
-                super.log(priority, tag, message, t)
-            }
-            logCache.add(
-                LogDesc(priority, tag, message, t)
-            )
-            logLock.withLock {
-                while (logCache.size > KEEP_LOGS) {
-                    logCache.removeFirst()
-                }
-            }
+        if (BuildConfig.DEBUG) {
+            super.log(priority, tag, message, t)
+        }
+        logCache.add(
+            LogDesc(priority, tag, message, t)
+        )
+        while (logCache.size > KEEP_LOGS) {
+            logCache.removeFirst()
         }
     }
 
     fun logToString(): Array<String> {
-        val logLines = runBlocking {
-            logLock.withLock {
-                logCache.map {
-                    val level = when (it.priority) {
-                        Log.ERROR -> "Error"
-                        Log.DEBUG -> "Debug"
-                        Log.WARN -> "Warn"
-                        Log.INFO -> "Info"
-                        Log.VERBOSE -> "Verbose"
-                        Log.ASSERT -> "Assert"
-                        else -> "Unknown"
-                    }
-                    val trace = it.throwable?.stackTraceToString() ?: ""
-                    return@map "${it.tag ?: ""}: $level ${it.message} $trace"
-                }.toTypedArray()
+        return logCache.map {
+            return@map try {
+                val level = when (it.priority) {
+                    Log.ERROR -> "Error"
+                    Log.DEBUG -> "Debug"
+                    Log.WARN -> "Warn"
+                    Log.INFO -> "Info"
+                    Log.VERBOSE -> "Verbose"
+                    Log.ASSERT -> "Assert"
+                    else -> "Unknown"
+                }
+                val trace = it.throwable?.stackTraceToString() ?: ""
+                "${it.tag ?: ""}: $level ${it.message} $trace"
+            } catch (e: NullPointerException) {
+                ""
             }
-        }
-        return logLines
+        }.toTypedArray()
     }
 }
